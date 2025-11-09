53ecd4259c4009418fe8d6155d06df6e1e4265d4
Jérémie Crinon <accounts@jcrinon.com>
2025-11-09 11:26:13 +0100

    Adding inputs in the grid and making them update the grid.

--- src/ui/sudoku_grid.rs
+++ src/ui/sudoku_grid.rs
@@ -3,7 +3,7 @@ use iced::{
     widget::{
         column,
         row,
-        text,
+        text_input,
         Column
     },
     Element
@@ -14,21 +14,56 @@ pub struct SudokuGrid {
     value: Grid,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 pub enum SudokuMessage {
-
+    InputChanged { x: usize, y: usize, value: String },
 }
 
 impl SudokuGrid {
-    pub fn update(&mut self, _message: SudokuMessage) {
+    pub fn update(&mut self, message: SudokuMessage) {
+        match message {
+            SudokuMessage::InputChanged { x, y, value } => {
+                // Parse the input to a number an make it 0 if it's not valid
+                let value_trimmed = value.trim();
 
+                let value_parsed = if !value_trimmed.is_empty() && value_trimmed.len() == 1 {
+                    value_trimmed.chars().next().and_then(|c| c.to_digit(10).map(|d| d as u8))
+                } else {
+                    None
+                };
+
+                if let Some(num) = value_parsed {
+                    self.value.set(x, y, num);
+                } else {
+                    self.value.set(x, y, 0);
+                }
+
+            }
+        }
     }
 
     pub fn view(&self) -> Element<'_, SudokuMessage> {
 
         let rows = (0..9).map(|y| {
             let cells = (0..9).map(|x| {
-                text(self.value.get(x, y)).into()
+                
+                // Convert the value to string and make it empty if it's 0 (0 = empty cell)
+                let value = self.value.get(x, y);
+                let value_displayed = if value != 0 {
+                    value.to_string()
+                } else {
+                    String::new()
+                };
+
+                text_input("", &value_displayed)
+                    .on_input({
+                        let x = x;
+                        let y = y;
+                        move |v| SudokuMessage::InputChanged {
+                            x, y, value: v,
+                        }
+                    })
+                    .into()
             });
 
             row(cells).into()
